33db805ad36af44d2e949cda0d9bc5a18ad7fa8c
MennoMarkus <45625943+MennoMarkus@users.noreply.github.com>
2018-12-31 18:47:22 +0100

    Moved utils to right location

--- include/asm/utils.h
+++ include/asm/utils.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "./types.h"
 
-extern "C" void startTimer();
-extern "C" u32 stopTimer();
+extern "C" void startTimer(); // Use noCashStartTimer and noCashStopTimer for better accuracy.
+extern "C" u32 stopTimer();   //
 extern "C" u32 lutDiv(u32 numerator, u32 denominator);
+
+// No$Gba emulator specific functions! Become useless outside of the emulator but won't crash.
+extern "C" char noCashPrintBuffer[80];
+extern "C" void noCashPrintFlush();
+extern "C" void noCashPrint(const char* str);
+
+inline void noCashStartTimer() { noCashPrint("TIMER START%zeroclks%"); }
+inline void noCashStopTimer() { noCashPrint("TIMER END: %lastclks% cycles"); }
